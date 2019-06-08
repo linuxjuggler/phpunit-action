3cb97008f9c4e9addde88fee92930c912b3d4f93
Zaher Ghaibeh <z@zah.me>
2019-06-08 18:19:45 +0300

    revert back to normal docker file

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,6 @@
 
 set -ex
 
-composer self-update
-
 composer global require hirak/prestissimo --prefer-dist --no-interaction --prefer-stable --no-suggest --no-progress
 
 composer install --prefer-dist --no-interaction --prefer-stable --no-suggest --no-progress
